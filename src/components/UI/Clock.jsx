f00b9819759d0f4e8dc06c20fea709dc3495b6dd
Peter <peterwchusid@gmail.com>
2023-03-07 09:40:18 -0500

    adjusted clock date

--- src/components/UI/Clock.jsx
+++ src/components/UI/Clock.jsx
@@ -11,7 +11,7 @@ const Clock = () => {
   let interval;
 
   const countDown = () => {
-    const destination = new Date("December 10, 2023").getTime();
+    const destination = new Date("April 10, 2023").getTime();
 
     interval = setInterval(() => {
       const now = new Date().getTime();
